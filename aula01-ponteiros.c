b44f6431f99525f83ce4bf0e25a47dca240e318b
Paulaadiogenes <paula.oliveira82202@alunos.ufersa.edu.br>
2026-08-17 20:26:55 -0300

    Correct pointer implementation in aula01-ponteiros.c
    Fix syntax errors and add pointer usage in the code.

--- aula01-ponteiros.c
+++ aula01-ponteiros.c
@@ -1,1 +1,11 @@
-.
+#include <studio.h> 
+#include <stdlib.h>
+int mains (void) {
+    int numero; 
+    int* ponteiro = NULL;
+    printf("digite o valor:");
+    scanf("%d", &numero);
+    ponteiro = &numero;
+    printf("numero=%d" \n ponteiro=%p);
+    return 0;
+}
